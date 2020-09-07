1852630f21d58b6b36422e9827f19b2577a3ac15
alexhkz <alexwebcode@gmail.com>
2020-09-07 19:44:52 +0300

    menu-close-prob

--- dist/js/script.js
+++ dist/js/script.js
@@ -9,3 +9,37 @@ hamburger.addEventListener('click', () => {
 closeElem.addEventListener('click', () => {
 	menu.classList.remove('active');
 });
+// "use strict";
+
+// window.addEventListener('DOMContentLoaded', () => {
+// 	function bindModal(trigger, modal, close) {
+// 		trigger.addEventListener('click', (e) => {
+// 			if (e.target) {
+// 				e.preventDefault();
+// 			}
+// 			// modal.style.display = 'block';
+// 			// document.body.style.overflow = 'hidden';
+// 			menu.classList.add('active');
+// 		});
+
+// 		close.addEventListener('click', () => {
+// 			// modal.style.display = 'none';
+// 			// document.body.style.overflow = '';
+// 			menu.classList.remove('active');
+// 		});
+
+// 		modal.addEventListener('click', (e) => {
+// 			if (e.target !== '.menu.active') {
+// 				// modal.style.display = 'none';
+// 				// document.body.style.overflow = '';
+// 				menu.classList.remove('active');
+// 			}
+// 		});
+// 	}
+
+// 	const hamburger = document.querySelector('.hamburger'),
+// 		menu = document.querySelector('.menu'),
+// 		closeElem = document.querySelector('.menu__close');
+
+// 	bindModal(hamburger, menu, closeElem);
+// });
